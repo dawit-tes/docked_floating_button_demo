3a91d6270fe7d275d743d05de8448bea39983421
Dawit <davortes@gmail.com>
2020-05-10 11:36:49 +0300

    adding bottom bar with notched shape

--- lib/main.dart
+++ lib/main.dart
@@ -10,39 +10,45 @@ class MyApp extends StatelessWidget {
 //        This specifies the location of the floating action button
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.white,
           elevation: 6,
           child: Icon(
             Icons.add,
             size: 30.0,
+            color: Colors.blueGrey.shade700,
           ),
           onPressed: () {},
         ),
         bottomNavigationBar: BottomAppBar(
-          color: Colors.blueGrey,
+          notchMargin: 8,
+          color: Colors.grey.shade500,
           shape: CircularNotchedRectangle(),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              BottomBarItemWidget(
-                icon: Icons.send,
-                onTap: () {},
-              ),
-              BottomBarItemWidget(
-                icon: Icons.share,
-                onTap: () {},
-              ),
-              SizedBox(
-                width: 30,
-              ),
-              BottomBarItemWidget(
-                icon: Icons.star,
-                onTap: () {},
-              ),
-              BottomBarItemWidget(
-                icon: Icons.info_outline,
-                onTap: () {},
-              ),
-            ],
+          child: Container(
+            height: 56,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                BottomBarItemWidget(
+                  icon: Icons.send,
+                  onTap: () {},
+                ),
+                BottomBarItemWidget(
+                  icon: Icons.share,
+                  onTap: () {},
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                BottomBarItemWidget(
+                  icon: Icons.star,
+                  onTap: () {},
+                ),
+                BottomBarItemWidget(
+                  icon: Icons.info_outline,
+                  onTap: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -57,7 +63,10 @@ class BottomBarItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      icon: Icon(icon),
+      icon: Icon(
+        icon,
+        color: Colors.blueGrey.shade700,
+      ),
       onPressed: onTap,
     );
   }
