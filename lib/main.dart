6a186f19357e7c257970e6e39e87f6cb8ed96375
Dawit <davortes@gmail.com>
2020-05-10 11:25:41 +0300

    adding bottom bar with notched shape

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        bottomNavigationBar: BottomAppBar(),
+//        This specifies the location of the floating action button
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          elevation: 6,
+          child: Icon(
+            Icons.add,
+            size: 30.0,
+          ),
+          onPressed: () {},
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blueGrey,
+          shape: CircularNotchedRectangle(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              BottomBarItemWidget(
+                icon: Icons.send,
+                onTap: () {},
+              ),
+              BottomBarItemWidget(
+                icon: Icons.share,
+                onTap: () {},
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              BottomBarItemWidget(
+                icon: Icons.star,
+                onTap: () {},
+              ),
+              BottomBarItemWidget(
+                icon: Icons.info_outline,
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class BottomBarItemWidget extends StatelessWidget {
+  BottomBarItemWidget({@required this.icon, @required this.onTap});
+  final IconData icon;
+  final Function onTap;
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(icon),
+      onPressed: onTap,
     );
   }
 }
